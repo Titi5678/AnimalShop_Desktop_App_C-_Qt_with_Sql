1700779e450794b375f190a4fa80e49139be7acf
louati <louati@localhost>
2020-02-08 12:36:45 +0100

    how to use commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+﻿
+
+#include <QApplication>
+#include<QLayout>
+#include<QWidget>
+#include<QPushButton>
+#include <QLineEdit>
+#include <QFormLayout>
+#include <QDialog>
+#include <QRadioButton>
+#include<QMessageBox>
+#include<QtWidgets>
+#include "principalwindow.h"
+#include"loginwindow.h"
+
+
+
+int main ( int argc , char * argv[])
+
+{
+
+   QApplication app(argc , argv);
+
+
+   LoginWindow *window ;
+   PrincipalWindow  *fennster = new PrincipalWindow;
+
+   window = new LoginWindow(fennster);
+    fennster->setwidgets(window) ;
+
+    fennster->show() ;
+    app.exec();
+
+
+}
